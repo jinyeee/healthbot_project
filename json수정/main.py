50a96c70813eab06dfb2eb6c119f08af7205170c
hayleeBae <148422669+hayleeBae@users.noreply.github.com>
2023-12-16 11:37:43 +0900

    Update main.py

--- json수정/main.py
+++ json수정/main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
